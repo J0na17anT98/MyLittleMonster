d201763b7868095e519a79cbf1fcb7c5eaae39ef
J0na17anT98 <pilotj@outlook.com>
2016-04-13 11:54:51 -0400

    added MonsterImg2.swift

---
+++ myLittleMonster/MonsterImg2.swift
@@ -0,0 +1,66 @@
+//
+//  MonsterImg2.swift
+//  myLittleMonster
+//
+//  Created by Jonathan Tsistinas on 4/13/16.
+//  Copyright © 2016 Techinator. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MonsterImg2: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        playAppearAnimation()
+    }
+    
+    func playAppearAnimation() {
+        
+        self.image = UIImage(named:"appear1.png")
+        
+        self.animationImages = nil
+        
+        
+        var imgArray = [UIImage]()
+        //swift 2.1 code for loop \/
+        //for var x = 1; x <= 10; x += 1 {
+        //swift 3.0 code for loop \/
+        for x in 1.stride(to: 10, by: 1) {
+            let img = UIImage(named: "appear\(x).png")
+            imgArray.append(img!)
+            
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 0
+        self.startAnimating()
+    }
+    
+    func playHideAnimation() {
+        
+        self.image = UIImage(named: "hide6.png")
+        
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        //swift 2.1 code for loop \/
+        //for var x = 1; x <= 6; x += 1 {
+        //swift 3.0 code for loop \/
+        for x in 1.stride(to: 6, by: 1) {
+            let img = UIImage(named: "hide\(x).png")
+            imgArray.append(img!)
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 1
+        self.startAnimating()
+    }
+}
+
